aa5a39923287bc4ed94386a848402217a94b7f2d
AidenKao <forclh@qq.com>
2024-01-29 08:39:38 +0800

    update

---
+++ code/00-Vue2相关/vue2生命周期/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Person v-if="isShow"/>
+</template>
+
+<script>
+import Person from './components/Person.vue'
+
+
+export default {
+  name: 'App',
+  components: { Person },
+  data() {
+    return {
+      isShow: true
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ code/00-Vue2相关/vue2生命周期/src/components/Person.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="person">
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="add">点我+1</button>
+  </div>
+</template>
+
+  
+<script>
+
+export default {
+  /* eslint-disable */
+  name: 'Person',
+  // 数据
+  data() {
+    return {
+      sum: 1
+    }
+  },
+  // 方法
+  methods: {
+    add() {
+      this.sum += 1
+    }
+  },
+
+  // 创建前的钩子
+  beforeCreate() {
+    console.log('创建前')
+  },
+  // 创建完毕的钩子
+  created() {
+    console.log('创建完毕')
+  },
+  // 挂载前的钩子
+  beforeMount() {
+    console.log('挂载前')
+  },
+  // 挂载完毕的钩子
+  mounted() {
+    console.log('挂载完毕')
+  },
+  // 更新前
+  beforeUpdate() {
+    console.log('更新前')
+  },
+  // 更新完毕
+  updated() {
+    console.log('更新完毕')
+  },
+  // 销毁前
+  beforeDestroy() {
+    console.log('销毁前')
+  },
+  // 销毁完毕
+  destroyed() {
+    console.log('销毁完毕')
+  }
+}
+
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 0 10px;
+}
+</style>
